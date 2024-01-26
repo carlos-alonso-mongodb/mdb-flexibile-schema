71fb8f1b393fb51531e5ba468d2184d745994c7c
carlos-alonso-mongodb <98921399+carlos-alonso-mongodb@users.noreply.github.com>
2024-01-26 15:06:13 +0100

    Add files via upload

---
+++ demo_settings.py
@@ -0,0 +1,5 @@
+URI_STRING = "YOUR_ATLAS_URI_STRING"
+# URI_STRING = "mongodb+srv://main_user:mypasswordtestcluster-abcd.mongodb.net/test?retryWrites=true"
+NUM_ITEMS = 10000
+NUM_SAMPLING = 1000
+COLLECTION_NAME = "First_Last"
